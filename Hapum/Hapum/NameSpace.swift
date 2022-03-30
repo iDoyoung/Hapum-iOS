b915e6b4e5f44df4a998d695e433d00cbed1488f
iDoyoung <ido.young@icloud.com>
2022-03-30 11:51:51 +0900

    feat: Add method to create album (#2)

---
+++ Hapum/Hapum/NameSpace.swift
@@ -0,0 +1,12 @@
+//
+//  NameSpace.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/03/30.
+//
+
+import Foundation
+
+enum NameSpace {
+    static let albumName = "Hapum"
+}
--- Hapum/Hapum/PhotosService.swift
+++ Hapum/Hapum/PhotosService.swift
@@ -13,8 +13,7 @@ class PhotosService {
     private let imageManager: PHImageManager!
     private let readWriteStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
     private var allPhotos: PHFetchResult<PHAsset>!
-    private var smartAlbums: PHFetchResult<PHAssetCollection>!
-    private var userCollections: PHFetchResult<PHCollection>!
+    private var userCollection: PHFetchResult<PHAssetCollection>!
     
     private lazy var allPhotosOptions: PHFetchOptions = {
         let options = PHFetchOptions()
@@ -27,14 +26,25 @@ class PhotosService {
     }
     
     func fetchPHAssets() {
-        self.allPhotos = PHAsset.fetchAssets(with: self.allPhotosOptions)
-        self.smartAlbums = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
-        self.userCollections = PHCollectionList.fetchTopLevelUserCollections(with: nil)
+        allPhotos = PHAsset.fetchAssets(with: self.allPhotosOptions)
+        
+        let options = PHFetchOptions()
+        options.predicate = NSPredicate(format: "title = %@", NameSpace.albumName)
+        userCollection = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: options)
+        if userCollection.firstObject == nil {
+            createAlbum()
+        }
     }
    
     func checkPhotosAccessStatus(completion: @escaping (PHAuthorizationStatus) -> Void) {
         PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
             completion(status)
+        }
+    }
+    
+    private func createAlbum() {
+        PHPhotoLibrary.shared().performChanges {
+            PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: NameSpace.albumName)
         }
     }
     
