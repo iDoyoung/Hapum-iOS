276e64496d60eb25decfaae964e86d0f6d436a62
iDoyoung <ido.young@icloud.com>
2022-03-31 12:43:24 +0900

    feat: Add method to request photos for assets (#3)

--- Hapum/Hapum/MainViewModel.swift
+++ Hapum/Hapum/MainViewModel.swift
@@ -9,17 +9,26 @@ import Photos
 
 class MainViewModel {
     
-    private let imageManager: PHImageManager!
+    private let imageManager: PHImageManager = PHImageManager.default()
     private let service: PhotosService
     
-    init(service: PhotosService, imageManager: PHImageManager) {
+    init(service: PhotosService) {
         self.service = service
-        self.imageManager = imageManager
     }
     
     //TODO: Add observed property
     ///tag - status
     ///tag - 14 today photos about all
     ///tag - photos from hapum album
+    
+    private func requestPhotos(for assets: PHFetchResult<PHAsset>) -> [Photo] {
+        var photos = [Photo]()
+        for index in 0..<assets.count {
+            imageManager.requestImage(for: assets[index], targetSize: .zero, contentMode: .default, options: nil) { image, _ in
+                photos.append(Photo(identifier: (assets[index].localIdentifier), image: image!))
+            }
+        }
+        return photos
+    }
     
 }
