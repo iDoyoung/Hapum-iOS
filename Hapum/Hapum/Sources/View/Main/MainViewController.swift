0abb34817b579c0493944f9d8ad3e58b26baaaae
iDoyoung <ido.young@icloud.com>
2022-04-02 23:17:03 +0900

    feat: Add MainViewController with IBOutlet (#4)

---
+++ Hapum/Hapum/Sources/View/Main/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/03/27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var todayPhotosView: UIView!
+    @IBOutlet var photosInAlbumCollectionView: UICollectionView!
+    @IBOutlet var statusMessageLabel: UILabel!
+    @IBAction func presentFrameSelection(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
