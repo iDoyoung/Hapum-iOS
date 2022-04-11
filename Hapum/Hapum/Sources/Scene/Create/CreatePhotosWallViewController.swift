5e0aa8c11c770719db17c004cde40998c95141e6
iDoyoung <ido.young@icloud.com>
2022-04-11 23:26:33 +0900

    feat: Implement Create photos wall view ui (#8)

---
+++ Hapum/Hapum/Sources/Scene/Create/CreatePhotosWallViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreatePhotosWallViewController.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/04/11.
+//
+
+import UIKit
+
+class CreatePhotosWallViewController: UIViewController {
+    
+    @IBOutlet weak var photosWallView: UIView!
+    @IBOutlet weak var lightViewSwitch: UISwitch!
+    @IBOutlet weak var changeLightColorButton: UIButton!
+    @IBOutlet weak var changePhotosFrameColorButton: UIButton!
+    @IBOutlet weak var changeBackgroundColorButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func onOffLightEffect(_ sender: UISwitch) {
+    }
+    @IBAction func changeColor(_ sender: UIButton) {
+    }
+    
+}
