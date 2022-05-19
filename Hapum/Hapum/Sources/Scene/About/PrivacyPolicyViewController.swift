7d8b7c135ac5770daa8cbb10df4f7382fd767588
iDoyoung <ido.young@icloud.com>
2022-05-19 12:39:27 +0900

    feat: Add web view to present Privacy policy (#24)

--- Hapum/Hapum/Sources/Scene/About/PrivacyPolicyViewController.swift
+++ Hapum/Hapum/Sources/Scene/About/PrivacyPolicyViewController.swift
@@ -6,24 +6,25 @@
 //
 
 import UIKit
+import WebKit
 
 class PrivacyPolicyViewController: UIViewController {
-
+    
+    @IBOutlet weak var privacyPolicyWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let myURL = URL(string: privacyPolicyURL)
+        let myRequest = URLRequest(url: myURL!)
+        privacyPolicyWebView.load(myRequest)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private var privacyPolicyURL: String {
+        if Locale.current.languageCode == "ko" {
+            return "https://hyper-stealer-69c.notion.site/b83a29f7c40445c1910e49c9dfd8792c"
+        } else {
+            return "https://hyper-stealer-69c.notion.site/Privacy-Policy-a4fd70c8d3874f068afe57a28b5fea51"
+        }
     }
-    */
-
+    
 }
---
+++ Hapum/Hapum/Sources/Util/Namespace.swift
@@ -0,0 +1,79 @@
+//
+//  NameSpace.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/03/30.
+//
+
+import Foundation
+
+enum StoryboardName {
+    static let main = "Main"
+    static let about = "About"
+    static let createPhotosWall = "Create"
+}
+
+enum ViewControllerID {
+    static let main = "MainViewController"
+    static let about = "AboutAppViewController"
+    static let createPhotosWall = "CreatePhotosWallViewController"
+}
+
+enum NibName {
+    static let photosViewCell = "PhotosViewCell"
+    static let frameViewCell = "FrameViewCell"
+    static let photosWallView = "PhotosWallView"
+}
+
+enum SegueID {
+    static let privacyPolicy = "PrivacyPolicy"
+}
+
+enum AlbumName{
+    static let hapum = "Hapum"
+}
+
+//MARK: - For UI
+enum LabelText {
+    static let recommending = "Recommend Hapum".localized
+    static let reviewing = "Write a review".localized
+    static let privacyPolicy = "Privacy policy".localized
+}
+//MARK: - Alert
+enum AlertTitle {
+    static let managingPhotosAccess = ""
+    static let savingInPhotos = ""
+    static let creatingFailure = "예기치 못한 오류로 이미지 저장에 실패하였습니다.".localized
+}
+
+enum AlertMessage {
+    static let managingPhotosAccess = "Manage to Access to Your Photos".localized
+    static let savingInPhotos = "Save in Photos".localized
+    static let creatingFailure = "개발자에게 오류를 알려주시면 감사하겠습니다.".localized
+}
+
+enum AlertActionTitle {
+    static let changeSetting = "Change Settings".localized
+    static let selectMore = "Select More Photos".localized
+    static let okay = "OK".localized
+    static let cancel = "Cancel".localized
+    static let save = "Save".localized
+}
+
+enum PhotosAccessStatusMessage {
+    static let needToSet = "Please Allow Access to Your Photos".localized
+    static let limitedStatus = "You allow access to selected only in your Photos".localized
+}
+
+//MARK: - Menu
+enum MenuTitle {
+    static let changeColor = "Change Color".localized
+    static let changePhoto = ""
+}
+
+enum MenuActionTitle {
+    static let choosePhoto = "Choose a photo".localized
+    static let takePhoto = "Take a photo".localized
+    static let changeBackgroundColor = "Background".localized
+    static let changeFrameColor = "Frame".localized
+}
