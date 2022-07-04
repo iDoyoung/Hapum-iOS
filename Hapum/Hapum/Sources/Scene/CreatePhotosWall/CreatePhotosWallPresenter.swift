a5a4ffc43a8d751d55755a80dedeb346ef7a5eb4
iDoyoung <ido.young@icloud.com>
2022-07-04 13:38:32 +0900

    Refactor Create photos wall presenter

--- Hapum/Hapum/Sources/Scene/CreatePhotosWall/CreatePhotosWallPresenter.swift
+++ Hapum/Hapum/Sources/Scene/CreatePhotosWall/CreatePhotosWallPresenter.swift
@@ -5,10 +5,11 @@
 //  Created by Doyoung on 2022/04/12.
 //
 
-import Foundation
+import UIKit
+import Photos
 
 protocol CreatePhotosWallPresentationLogic {
-    func presentPhotos(resource: [Photos.Asset])
+    func presentPhotos(resource: PHFetchResult<PHAsset>?)
     func showCameraTypeImagePicker()
     func showDoneAlert() 
     func showCreatingSuccess()
@@ -20,7 +21,30 @@ final class CreatePhotosWallPresenter: CreatePhotosWallPresentationLogic {
     
     weak var viewController: CreatePhotosWallDisplayLogic?
     
-    func presentPhotos(resource: [Photos.Asset]) {
+    private let imageManager = PHImageManager.default()
+    
+    private func requestImages(for assets: PHFetchResult<PHAsset>) -> [UIImage] {
+        var images = [UIImage]()
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
+        
+        for index in 0..<assets.count {
+            let asset = assets[index]
+            imageManager.requestImage(for: asset, targetSize: .zero, contentMode: .default, options: requestOptions) { image, _ in
+                if let image = image {
+                    images.append(image)
+                }
+            }
+        }
+        
+        return images
+    }
+    
+    func presentPhotos(resource: PHFetchResult<PHAsset>?) {
+        guard let resource = resource else {
+            return
+        }
+        let images = requestImages(for: resource)
         viewController?.displayPhotos(viewModel: resource)
     }
     
