8f53ed8b2d04e6cd7fa00a40def5f4a6a4403350
iDoyoung <ido.young@icloud.com>
2022-07-10 18:14:20 +0900

    Add View controller for create photos wall template

---
+++ Hapum/Hapum/Sources/Scene/CreatePhotosWallTemplate/CreatePhotosWallTemplateViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CreatePhotosWallTemplateViewController.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/07/10.
+//
+
+import UIKit
+
+class CreatePhotosWallTemplateViewController: UIViewController {
+
+    @IBOutlet weak var wallBackgroundView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+    @IBAction func addFrame(_ sender: UIButton) {
+    }
+    
+}
