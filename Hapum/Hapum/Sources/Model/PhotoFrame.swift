485faad9b2851930210eb3dedc8fc8a6d5707e62
iDoyoung <ido.young@icloud.com>
2022-07-10 18:39:52 +0900

    Add Photo Frame model

---
+++ Hapum/Hapum/Sources/Model/PhotoFrame.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoFrame.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/07/10.
+//
+
+import UIKit
+
+struct PhotoFrame {
+    var frame: CGRect
+    var borderWidth: CGFloat
+    var space: Bool
+}
