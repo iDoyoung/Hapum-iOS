7c678d8c9e6876d678abd664fd744c73f59e27f3
iDoyoung <ido.young@icloud.com>
2022-07-10 19:05:46 +0900

    Add Interactor for Create photos wall template

---
+++ Hapum/Hapum/Sources/Scene/CreatePhotosWallTemplate/CreatePhotosWallTemplateInteractor.swift
@@ -0,0 +1,30 @@
+//
+//  CreatePhotosWallTemplateInteractor.swift
+//  Hapum
+//
+//  Created by Doyoung on 2022/07/10.
+//
+
+import Foundation
+
+protocol CreatePhotosWallTemplateBusinessLogic {
+    func addPhotoFrame(_ photoFrame: PhotoFrame)
+    func createPhotosWallTemplate()
+}
+
+protocol CreatePhotosWallTemplateDataStore {
+    var photoFrames: [PhotoFrame] { get }
+}
+
+final class CreatePhotosWallTemplateInteractor: CreatePhotosWallTemplateBusinessLogic, CreatePhotosWallTemplateDataStore {
+    
+    var photoFrames = [PhotoFrame]()
+    var presenter: CreatePhotosWallTemplatePresentationLogic?
+    
+    func addPhotoFrame(_ photoFrame: PhotoFrame) {
+    }
+    
+    func createPhotosWallTemplate() {
+    }
+    
+}
